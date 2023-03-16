aa6386d0c286217a880eb0af1e1f2363277397a1
Carlos Severiche <carloseveriche1716@gmail.com>
2023-03-15 23:01:21 -0500

    funcion crearUsuario

--- index.js
+++ index.js
@@ -10,4 +10,42 @@ class Usuario{
         this.pais = pais;
         this.edad = edad;
     }
+
+    cambiarNombre(){
+        nuevoNombre = prompt(`Ingresa tu nuevo nombre: `);
+        this.nombre = nuevoNombre;
+    }
 }
+
+
+function crearUsuario(){
+
+    let cantidadRegistros = parseInt(prompt(`¿Cuántas personas deseas registar?`))
+
+    const USUARIOS = [];
+
+    if(cantidadRegistros == 0){
+        alert(`Ingrese una cantidad minima de 1`)
+    }
+
+    else{
+        
+        for( let i = 0 ; i < cantidadRegistros ; i++){  
+
+        
+    
+        let nombre = prompt(`Ingresa tu nombre: `);
+        let apellido = prompt(`Ingresa tu apellido: `);
+        let identificacion = prompt(`Ingresa tu numero de ID:`);
+        let pais = prompt(`Ingresa tu nacionalidad`);
+        let edad = parseInt(prompt(`Ingresa tu edad`));
+    
+        let nuevoUsuario = new Usuario(nombre, apellido, identificacion, pais, edad);
+    
+        USUARIOS.push(nuevoUsuario);
+        console.log(USUARIOS);}
+    
+
+    }
+}
+crearUsuario();
