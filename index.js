f42340505c8942ff03198757223dc1f658b7caa8
Carlos Severiche <carloseveriche1716@gmail.com>
2023-03-15 21:36:34 -0500

    class usuario

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+
+
+class Usuario{
+    constructor (nombre, apellido, identificacion, pais, edad){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.identificacion = identificacion;
+        this.pais = pais;
+        this.edad = edad;
+    }
+}
